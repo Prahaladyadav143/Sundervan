7871275d0faa5df1ca18c6088ece291244569d48
Prahaladyadav143 <prahad.yadav@gmail.com>
2025-08-03 23:23:20 +0530

    bahubali

---
+++ terra.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "09098ad9-99a1-43f6-9b36-7c1d79f03953"
+}
+
+resource "azurerm_resource_group" "madhuvan" {
+  name     = "ramu-rg"
+  location = "West Europe"
+
+}
+# jgjhghjghj
+# shjhahs
+# hjsgghkd
