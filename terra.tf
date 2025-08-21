a4f9635f73531696a2af4d28e689c071fe594260
Prahaladyadav143 <prahad.yadav@gmail.com>
2025-08-21 11:02:43 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.38.1"
+      version = "4.40.0"
     }
   }
 }
 
 provider "azurerm" {
     features {}
-    subscription_id = "09098ad9-99a1-43f6-9b36-7c1d79f03953"
+    subscription_id = "b3b6b7ed-0b15-49ca-b3ce-092e07ca3fef"
 }
 
 resource "azurerm_resource_group" "madhuvan" {
